6367f8aab67cc2849d15f1dcf93db32acd68de6c
Jordan Carver <jordan_carver@hotmail.com>
2020-10-12 01:20:33 -0600

    added delete function and route by id

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ function createNewNote(body, noteArray) {
 };
 
 // Validate note
-function validateNote(note) {
+function validateNewNote(note) {
     if (!note.title || typeof note.title !== 'string') {
       return false;
     }
@@ -37,6 +37,20 @@ function validateNote(note) {
       return false;
     }
     return true;
+};
+
+// Delete note
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        if (id === notesArray[i].id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify({ notes: notesArray }, null, 2)
+            );
+        }
+    }
+    return false;
 };
 
 // Routes
@@ -51,7 +65,7 @@ app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
 
     // if any data in req.body is incorrect, send 404 error back
-    if (!validateNote(req.body)) {
+    if (!validateNewNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
     } else {
     // add note to json file & notes array in this function
@@ -69,6 +83,12 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+// route to delete note
+app.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+    res.json(notes);
+})
 
 // Listener
 app.listen(PORT, () => {
