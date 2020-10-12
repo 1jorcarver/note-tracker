8d998487e621520a227d21723853915d3ab01a8d
Jordan Carver <jordan_carver@hotmail.com>
2020-10-12 00:05:39 -0600

    required data notes and added route api/notes from index.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { notes } = require('./db/db.json');
 
 // Initialize Express
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// Routes
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+})
 
 
 // Listener
